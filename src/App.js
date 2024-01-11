d5d6e96740991f5ff96abaa4503888c148d27880
Minsa111 <dragonriderrgt@gmail.com>
2024-01-12 00:38:38 +0700

    try

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import HomePage from '../../minsa/src/Pages/Home';
 import AboutPage from './Pages/About';
 import '../../minsa/src/Assets/style.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path = "/" element = {<HomePage />}/>
         <Route path = "/about" element = {<AboutPage />}/>
       </Routes>
-    </Router>
   );
 }
 
